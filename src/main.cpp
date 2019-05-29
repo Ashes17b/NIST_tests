2e9bf3549b337c8cd7c18a8da4434f13d5a88dbc
Anton Boltach <a.boltach@pixelplex.io>
2019-05-29 21:07:28 +0300

    Corrected Timoshenko tests

--- src/main.cpp
+++ src/main.cpp
@@ -6,63 +6,63 @@ using namespace random_excursions_test;
 
 int main()
 {
-    // std::cout << "----------------------------------------------------------------------" << std::endl;
+    std::cout << "----------------------------------------------------------------------" << std::endl;
 
-    // Runs_test_boltach r_test_boltach;
-
-    // auto t1 = std::chrono::high_resolution_clock::now();
-    // r_test_boltach.read("../seq/seq8.bin");
-    // auto t2 = std::chrono::high_resolution_clock::now();
-
-    // auto duration_read = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() * 1e-6;
-    // printf("Time spent on read(seconds): %.8f\n", duration_read);
-
-    // t1 = std::chrono::high_resolution_clock::now();
-    // auto p_value = r_test_boltach.run_test();
-    // t2 = std::chrono::high_resolution_clock::now();
-    // auto duration_task = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() * 1e-6;
-    // printf("Time spent on task(seconds): %.8f\n", duration_task);
-
-    // printf("Time spent on everything(seconds): %.8f\n", duration_read + duration_task);
-
-    // printf("\nP-value runs_test: %.8f\n", p_value);
-
-    // std::cout << "----------------------------------------------------------------------" << std::endl;
-
-    // Random_excursion_variant_test_boltach r_e_v_test_boltach;
-
-    // t1 = std::chrono::high_resolution_clock::now();
-    // r_e_v_test_boltach.read("../seq/seq8.bin");
-    // t2 = std::chrono::high_resolution_clock::now();
-
-    // duration_read = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() * 1e-6;
-    // printf("Time spent on read(seconds): %.8f\n", duration_read);
-
-    // t1 = std::chrono::high_resolution_clock::now();
-    // p_value = r_e_v_test_boltach.run_test();
-    // t2 = std::chrono::high_resolution_clock::now();
-    // duration_task = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() * 1e-6;
-    // printf("Time spent on task(seconds): %.8f\n", duration_task);
-
-    // printf("Time spent on everything(seconds): %.8f\n", duration_read + duration_task);
-
-    // printf("\nP-value runs_test: %.8f\n", p_value);
-
-    // std::cout << "----------------------------------------------------------------------" << std::endl;
-
-    Runs_test_timoshenko r_test_timoshenko;
+    Runs_test_boltach r_test_boltach;
 
     auto t1 = std::chrono::high_resolution_clock::now();
-    r_test_timoshenko.read("../seq/seq8.bin");
+    r_test_boltach.read("../seq/seq7.bin");
     auto t2 = std::chrono::high_resolution_clock::now();
 
     auto duration_read = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() * 1e-6;
     printf("Time spent on read(seconds): %.8f\n", duration_read);
 
     t1 = std::chrono::high_resolution_clock::now();
-    auto p_value = r_test_timoshenko.run_test();
+    auto p_value = r_test_boltach.run_test();
     t2 = std::chrono::high_resolution_clock::now();
     auto duration_task = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() * 1e-6;
+    printf("Time spent on task(seconds): %.8f\n", duration_task);
+
+    printf("Time spent on everything(seconds): %.8f\n", duration_read + duration_task);
+
+    printf("\nP-value runs_test: %.8f\n", p_value);
+
+    std::cout << "----------------------------------------------------------------------" << std::endl;
+
+    Random_excursion_variant_test_boltach r_e_v_test_boltach;
+
+    t1 = std::chrono::high_resolution_clock::now();
+    r_e_v_test_boltach.read("../seq/seq7.bin");
+    t2 = std::chrono::high_resolution_clock::now();
+
+    duration_read = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() * 1e-6;
+    printf("Time spent on read(seconds): %.8f\n", duration_read);
+
+    t1 = std::chrono::high_resolution_clock::now();
+    p_value = r_e_v_test_boltach.run_test();
+    t2 = std::chrono::high_resolution_clock::now();
+    duration_task = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() * 1e-6;
+    printf("Time spent on task(seconds): %.8f\n", duration_task);
+
+    printf("Time spent on everything(seconds): %.8f\n", duration_read + duration_task);
+
+    printf("\nP-value runs_test: %.8f\n", p_value);
+
+    std::cout << "----------------------------------------------------------------------" << std::endl;
+
+    Runs_test_timoshenko r_test_timoshenko;
+
+    t1 = std::chrono::high_resolution_clock::now();
+    r_test_timoshenko.read("../seq/seq7.bin");
+    t2 = std::chrono::high_resolution_clock::now();
+
+    duration_read = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() * 1e-6;
+    printf("Time spent on read(seconds): %.8f\n", duration_read);
+
+    t1 = std::chrono::high_resolution_clock::now();
+    p_value = r_test_timoshenko.run_test();
+    t2 = std::chrono::high_resolution_clock::now();
+    duration_task = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() * 1e-6;
     printf("Time spent on task(seconds): %.8f\n", duration_task);
 
     printf("Time spent on everything(seconds): %.8f\n", duration_read + duration_task);
@@ -74,7 +74,7 @@ int main()
     Random_excursions_test_timoshenko r_e_test_timoshenko;
 
     t1 = std::chrono::high_resolution_clock::now();
-    r_e_test_timoshenko.read("../seq/seq8.bin");
+    r_e_test_timoshenko.read("../seq/seq7.bin");
     t2 = std::chrono::high_resolution_clock::now();
 
     duration_read = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() * 1e-6;
